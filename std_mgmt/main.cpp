41e79c8a93aafa858599e2b2e90a0c0a627a7cf7
Mahesh Regmi <geekyarthurs@gmail.com>
2020-04-17 19:36:22 +0545

    fixed null terminators

--- std_mgmt/main.cpp
+++ std_mgmt/main.cpp
@@ -23,6 +23,7 @@ int main()
         int roll;
         string studentName;
         int choice;
+        char choice_char = 'y';
         cout << "1. View" << endl
              << "2. Add" << endl
              << "3. Delete" << endl
@@ -34,15 +35,22 @@ int main()
             listStudent();
             break;
         case 2:
-            cin.ignore(300000, '\n');
-            cin.sync();
-            cin.clear();
-            cout << "Enter Name: ";
-            getline(cin, studentName);
-            cout << "Enter Roll: ";
-            cin >> roll;
-            stdx = {studentName, roll};
-            addStudent(stdx);
+            
+            while (choice_char == 'y')
+            {
+                cin.ignore(300000, '\n');
+                cin.sync();
+                cin.clear();
+                cout << "Enter Name: ";
+                getline(cin, studentName);
+                cout << "Enter Roll: ";
+                cin >> roll;
+                stdx = {studentName, roll};
+                addStudent(stdx);
+
+                cout << "Wanna Add Another? [y/n]";
+                cin >> choice_char;
+            }
             break;
         case 3:
 
@@ -75,7 +83,7 @@ void deleteStudent(int roll)
 
     data_out_file.open("data2.dat", ios::in | ios::out | ios::trunc);
     data_file.open("data.dat", ios::in | ios::out);
-    bool done = false;
+    
     while (!data_file.eof())
     {
 
@@ -92,13 +100,12 @@ void deleteStudent(int roll)
 
         if (roll_s != roll)
         {
-            done = true;
+            
             data_out_file << roll_s << "," << raw_str << endl;
         }
     }
 
-    if (done == true)
-        data_out_file << "\b";
+    
 
     data_file.close();
     data_out_file.close();
@@ -115,21 +122,21 @@ void listStudent()
 
     int roll_s;
     string raw_str;
-
+    cout << setw(17) << "ROLL NO" << setw(18) << "NAME" << endl;
     while (!file.eof())
     {
         getline(file, raw_str, ',');
-
+        
         if (!raw_str.empty())
         {
             roll_s = stoi(raw_str);
             getline(file, raw_str); //name
-            cout << setw(17) << "ROLL NO" << setw(18) << "NAME" << endl;
+            
             cout << setw(15) << roll_s << setw(20) << raw_str << endl;
         }
         else
         {
-            cout << "-----File is Empty!!------" << endl;
+            continue;
         }
     }
 }
